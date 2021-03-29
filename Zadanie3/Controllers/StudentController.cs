1bee51c007c07812f0eb7e56b15598e9a5eed1ba
s19317 <s19317@pjwstk.edu.pl>
2021-03-29 21:34:26 +0200

    Commited

---
+++ Zadanie3/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using Zadanie3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("{indexNumber}")]
+        public async Task<IActionResult> GetStudents(String indexNumber)
+        {
+            var pathCSV = @".\Data\Data.csv";
+            var hash = new HashSet<Student>(new StudentsCompare());
+            var log = new StringBuilder("");
+            var lines = System.IO.File.ReadAllLinesAsync(pathCSV);
+
+            foreach (var line in await lines)
+            {
+                var info = line.Split(",");
+
+                var student = new Student(line);
+                if (student.indexNumber == indexNumber)
+                {
+                    return Ok(line);
+                }
+                else
+                {
+                    return Ok(" ");
+                }
+            }
+            return Ok(" ");
+        }
+
+        //HttpPatch 
+        [HttpGet]
+        public async Task<IActionResult> GetStudentsAsync()
+        {
+            String dane = await System.IO.File.ReadAllTextAsync(@".\Data\Data.csv");
+
+            return Ok(dane);
+
+        }
+        //HttpPost
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.indexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        //HttpPut
+        [HttpPut]
+        public IActionResult UpdateStudent(Student student)
+        {
+            return Ok("Aktualizacja dokonczona");
+        }
+        //HttpDelete
+        [HttpDelete]
+        public IActionResult DeleteStudent(Student student)
+        {
+            return Ok("Usuwanie dokonczone");
+        }
+
+        //https://localhost:44339/api/students/?indexNumber=s1234
+
+
+    }
+}
---
+++ Zadanie3/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Zadanie3.Models
+{
+    public class Student
+    {
+        public string indexNumber { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string birthdate { get; set; }
+        public string email { get; set; }
+        public string mothersName { get; set; }
+        public string fathersName { get; set; }
+
+
+
+
+        public Student(string info)
+        {
+            var tab = info.Split(",");
+
+            fname = tab[0];
+            lname = tab[1];
+            indexNumber = "s" + tab[4];
+            birthdate = DateTime.Parse(tab[5]).ToString("dd/MM/yyyy");
+            email = tab[6];
+            mothersName = tab[7];
+            fathersName = tab[8];
+        }
+
+     
+        public override string ToString()
+        {
+            return $"{fname} {lname} {indexNumber}";
+        }
+
+    }
+}
